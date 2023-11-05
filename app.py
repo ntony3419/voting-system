11ac035508121f6af7b645766e9d70294a37d163
tony <ntony3419@gmail.com>
2023-11-05 13:45:38 +1030

    add content and function for login and template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+# TODO: import library for mongodb connection
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,15 +10,39 @@ def home():
 def login():
     username = request.form['username']
     password = request.form['password']
-    # Here you would add logic to verify the username and password.
-    # For now, we'll just redirect to the home page.
+    # TODO : verify login information and redirect to dashboard if all is correect
     return redirect(url_for('home'))
 
 @app.route('/forgot-password')
 def forgot_password():
-    # Here you would add logic for a password reset.
-    # For now, we'll just redirect to the home page.
+    # TODO : logic for reset password
+    # for now, we'll just redirect to the home page.
     return redirect(url_for('home'))
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/vote')
+def vote():
+    #fetch candidates from MongoDB
+    candidates = fetch_candidates()
+    return render_template('vote.html', candidates=candidates)
+
+@app.route('/add-candidate', methods=['GET', 'POST'])
+def add_candidate():
+    if request.method == 'POST':
+        # TODO logic to add a candidate to MongoDB
+        pass
+    return render_template('add_candidate.html')
+
+@app.route('/logout')
+def logout():
+    # TODO: end all session al return to signin page
+    return redirect(url_for('home'))
+
+def fetch_candidates():
+    pass
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
