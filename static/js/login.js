201e3392dbc51d52db667f5f0ef7d2a54a0718a7
tony <ntony3419@gmail.com>
2023-11-13 23:55:04 +1030

    update register

--- static/js/login.js
+++ static/js/login.js
@@ -24,11 +24,50 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     // register button
     const registerBtn = document.getElementById('register-btn');
-    const registerPopup = document.getElementById('register-popup');
-
-    if (registerBtn && registerPopup) {
+    if (registerBtn) {
         registerBtn.addEventListener('click', function() {
-            registerPopup.style.display = 'block';
+            // Load the registration form and display it as a popup
+            fetch('/path-to-register-html') // Adjust the path to your register.html
+                .then(response => response.text())
+                .then(html => {
+                    document.body.insertAdjacentHTML('beforeend', html);
+                    attachRegisterFormListener();
+                })
+                .catch(error => console.error('Error loading registration form:', error));
         });
     }
 });
+function attachRegisterFormListener() {
+    const registerForm = document.getElementById('register-form');
+    if (registerForm) {
+        registerForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const userData = {
+                username: document.getElementById('username').value,
+                password: document.getElementById('password').value,
+                email: document.getElementById('email').value,
+                phone: document.getElementById('phone').value
+            };
+    
+            fetch('/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Registration Success:', data);
+                //TODO: show pop up message register successfully
+                registerPopup.style.display = 'none';
+            })
+            .catch((error) => {
+                console.error('Registration Error:', error);
+                // TODO: handle errors 
+            });
+        });
+    } else {
+        console.error('Register form not found');
+    }
+}
