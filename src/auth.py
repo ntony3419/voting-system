611783a137c1a72f0f3b23f3f27dd61c4f232a28
tony <ntony3419@gmail.com>
2023-11-15 18:38:55 +1030

    update

--- src/auth.py
+++ src/auth.py
@@ -7,7 +7,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user_id' not in session:
-            return redirect(url_for('login'))
+            return redirect(url_for('handle_login'))
         return f(*args, **kwargs)
     return decorated_function
 
@@ -17,7 +17,7 @@ def role_required(*roles):
         def decorated_function(*args, **kwargs):
             user_id = session.get('user_id')
             if not user_id:
-                return redirect(url_for('login'))
+                return redirect(url_for('handle_login'))
             user_data = Database.find_by_id(user_id) 
             user = User.from_dict(user_data)
             if not user or not user.has_permission(*roles):
