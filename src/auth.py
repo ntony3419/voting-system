dde913e3539afd186f96e5e7fdb15bdd114ce3c2
tony <ntony3419@gmail.com>
2023-11-15 16:03:19 +1030

    update

--- src/auth.py
+++ src/auth.py
@@ -12,14 +12,20 @@ def login_required(f):
 
         # Check for session timeout due to inactivity
         last_activity = datetime.fromisoformat(session.get('last_activity'))
-        if last_activity is not None:
+        if isinstance(last_activity, str):
             last_activity_datetime = datetime.strptime(last_activity, '%Y-%m-%dT%H:%M:%S.%f')
-            if datetime.now() - last_activity_datetime > app.permanent_session_lifetime:
-                session.clear()
-                flash('Session timed out')
-                return redirect(url_for('login'))
+        elif isinstance(last_activity, datetime):
+            last_activity_datetime = last_activity
+        else:
+          
+            raise TypeError('Unexpected data type for last_activity in session.')
+
+        #heck for session timeout due to inactivity
+        if (datetime.now() - last_activity_datetime) > current_app.permanent_session_lifetime:
+            #  timed out
+            return redirect(url_for('logout'))
         
-        # Update last activity time
+        #Update last activity time
         session['last_activity'] = datetime.now().isoformat()
         return f(*args, **kwargs)
     return decorated_function
